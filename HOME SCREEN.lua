96f33d39ff85978622430e70dbca444990a42ea9
Swapy <123993214+Opqsxw888@users.noreply.github.com>
2024-05-28 20:47:54 +0200

    Update HOME SCREEN.lua

--- HOME SCREEN.lua
+++ HOME SCREEN.lua
@@ -1,12 +1,12 @@
--- Références aux éléments GUI
+
 local homeScreen = game.StarterPack.HomeScreen
-local settingsButton = homeScreen.Frame.SettingsButton -- Remplace par le bouton pour ouvrir les paramètres
+local settingsButton = homeScreen.Frame.SettingsButton 
 local settingsScreen = game.StarterPack.SettingsScreen
 
--- Fonction pour ouvrir l'écran de paramètres
+
 local function openSettings()
     settingsScreen.Enabled = true
-    homeScreen.Enabled = false -- Désactiver l'écran d'accueil
+    homeScreen.Enabled = false 
 end
 
 settingsButton.Activated:Connect(openSettings)
